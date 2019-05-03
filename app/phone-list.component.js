f8a3af5e243227bc8e6f91149b43d1b36d39e95f
Dmitry Belokursky <dbelokursky@gmail.com>
2019-05-03 22:10:19 +0500

    Tutorial 3 - Components

--- app/phone-list.component.js
+++ app/phone-list.component.js
@@ -5,6 +5,7 @@ angular.
   module('phonecatApp').
   component('phoneList', {
     template:
+    '<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
         '<ul>' +
           '<li ng-repeat="phone in $ctrl.phones">' +
             '<span>{{phone.name}}</span>' +
