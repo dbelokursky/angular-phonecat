acf4fa5ef1f19bd1f77d67bc7d6beccb2ce4ba95
Dmitry Belokursky <dbelokursky@gmail.com>
2019-05-05 17:40:19 +0500

    Tutorial 12 - Event Handlers

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -13,6 +13,10 @@ angular.
           self.mainImageUrl = imageUrl;
         };
 
+        self.onDblclick = function onDblclick(imageUrl) {
+          alert('You double-clicked image: ' + imageUrl);
+        };
+
         $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
           self.phone = response.data;
           self.setImage(self.phone.images[0]);
